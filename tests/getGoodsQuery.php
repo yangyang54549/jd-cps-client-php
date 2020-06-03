303dbb7264848cc6e64a24721da1296001b22e0b
yang <php_programmer@163.com>
2020-06-03 09:16:21 +0800

    删除测试用参数

--- tests/getGoodsQuery.php
+++ tests/getGoodsQuery.php
@@ -8,8 +8,8 @@ require_once dirname(__FILE__)."/../vendor/autoload.php";
 use Jd\Sdk\Client;
 use Jd\Sdk\Request\JdApiGoodsGetCategory;
 
-$appkey = 'd267d2fcbd82456e3571c291ea84d7a4';
-$secretkey = '3c67043089ca48f79fd23a11635540b7';
+$appkey = '';
+$secretkey = '';
 
 $client = new Client($appkey, $secretkey);
 $request = new JdApiGoodsGetCategory();
